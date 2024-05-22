3ac6371c5047ae62d175816b75862437e276c7f4
Mohammad.YM <Mymah592@gmail.com>
2024-05-21 23:10:18 -0400

    Update query_1.sql
    working around HW feedback

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,5 +1,5 @@
 -- This Query creates an empty table for Actors
-CREATE TABLE mymah592.Actors (
+CREATE OR REPLACE TABLE mymah592.Actors (
     actor VARCHAR,
     actor_ID VARCHAR,
     films ARRAY<ROW(Year INTEGER, film VARCHAR, votes INTEGER, rating DOUBLE, film_id VARCHAR)>,
