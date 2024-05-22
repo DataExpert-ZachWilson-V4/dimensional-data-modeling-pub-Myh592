466069d956b2f3bb3807c268e88be3bbafabbb6e
Mohammad.YM <Mymah592@gmail.com>
2024-05-21 22:22:13 -0400

    query_5.sql
    Query 5 submission based on following Lab 2 exercise

---
+++ submission/query_5.sql
@@ -0,0 +1,99 @@
+Insert INTO mymah592.actors_history_scd
+WITH
+    last_year_scd AS(
+    Select  
+         *
+    FROM
+        mymah592.actors_history_scd
+        WHERE
+        current_year = 2001
+    ),
+    current_year_scd AS(
+        SELECT
+        *
+        FROM 
+        mymah592.actors_history_scd
+        WHERE
+        current_year = 2002
+    ),
+    Combined AS(
+        SELECT
+        COALESCE(ly.actor_ID, cy.actor_ID) AS actor_ID,
+        COALESCE(ly.start_year, cy.start_year) AS start_year,
+        COALESCE(ly.end_year, cy.end_year) AS end_year,
+        CASE
+          WHEN ly.is_active <> cy.is_active THEN 1
+          WHEN ly.is_active = cy.is_active THEN 0
+          END as did_change
+          ly.is_active as is_active_last_year,
+          cy.is_active as is_active_this_year,
+          2002 as current_year
+    FROM
+        last_year_scd ly
+        FULL OUTER JOIN current_year_scd cy ON ly.actor_ID = cy.actor_ID
+        and ly.end_year + 1 = cy.current_year
+    ),
+    Changes AS(
+        SELECT
+          actor_ID,
+          current_year,
+          CASE
+            When did_change = 0 THEN ARRAY[
+                CAST(
+                    ROW(
+                        is_active_last_year,
+                        start_year,
+                        end_year +1,
+                        ) AS ROW(
+                            is_active BOOLEAN,
+                            start_year INTEGER,
+                            end_year INTEGER
+                        )
+                )
+            ]
+            WHEN did_change = 1 THEN ARRAY[
+                CAST(
+                    ROW(is_active_last_year, start_year, end_year) AS ROW(
+                        is_active BOOLEAN,
+                        start_year INTEGER,
+                        end_year INTEGER
+                    )
+                ),
+                CAST(
+                    ROW(
+                        is_active_this_year,
+                        current_year,
+                        current_year
+                    ) AS ROW(
+                        is_active BOOLEAN,
+                        start_year INTEGER,
+                        end_year INTEGER
+                    )
+                )
+            ]
+            WHEN did_change Change IS NULL Then ARRAY[
+                CAST(
+                    ROW(
+                        COALESCE(is_active_last_year, is_active_this_year),
+                        start_year,
+                        end_year
+                    ) AS ROW(
+                        is_active BOOLEAN,
+                        start_year INTEGER,
+                        end_year INTEGER
+                    )
+                )
+            ]
+            END as Change_Array
+            from
+            combined
+    )
+    SELECT
+    actor_id,
+    ARR.is_active,
+    ARR.start_year,
+    ARR.end_year,
+    current_year
+    FROM
+    changes
+    CROSS JOIN UNNEST (Change_Array) as ARR
