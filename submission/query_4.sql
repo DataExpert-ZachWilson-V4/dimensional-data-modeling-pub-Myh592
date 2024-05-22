a4caa0d4009658f1b05caee5143f0c5ec55cf0cf
Mohammad.YM <Mymah592@gmail.com>
2024-05-22 00:40:50 -0400

    Update query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -24,7 +24,7 @@ SELECT
 )
 -- Determining how many years active as well as which years are start and end of activity 
 Select actor_id, Streak_identifier,
-    MAX(quality_class) as quality_class,
+    quality_class,
     MAX(is_active) = 1 as is_active, -- this has been changed back into a boolen per WEEK 1 Lab 2
     MIN(current_year) as start_year,
     MAX(current_year) as end_year,
