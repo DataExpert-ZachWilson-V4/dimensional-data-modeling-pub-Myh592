971077b47255a267b94bb325f974f2e967ff738f
Mohammad.YM <Mymah592@gmail.com>
2024-05-21 22:21:04 -0400

    query_3.sql
    Attempting to submit HW1

---
+++ submission/query_3.sql
@@ -0,0 +1,13 @@
+Creat table mymah592.actors_history_scd (
+    actor VARCHAR,
+    actor_id VARCHAR,
+    quality_class VARCHAR,
+    is_active BOOLEAN,
+    Start_date INTEGER,
+    End_Date   INTEGER,
+    current_year INTEGER
+)
+WITH (
+    format ='PARQUET'
+partitioning = ARRAY['current_year']
+)
