8aea6c634c9bb6a7ca06a2c81b834dc8cb9142cb
Mohammad.YM <Mymah592@gmail.com>
2024-05-21 22:41:26 -0400

    query_4.sql
    Including Quality_class logic

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -10,17 +10,21 @@ LAG(quality_class,1 ) OVER (PARTITION by actor_id ORDER BY current_year) as qual
 current_year
 From mymah592.actors
 WHERE current_year <= 1999
-),
+)
 -- breaking out streak case statement on its own for neatness
 Streaked AS(
 SELECT
   *,
-  SUM(CASE WHEN is_active <> is_active_last_year THEN 1 ELSE 0 END)
+  SUM(CASE 
+        WHEN is_active <> is_active_last_year THEN 1
+        WHEN quality_class <> quality_class_last_year THEN 1 
+      ELSE 0 END)
     OVER(PARTITION BY actor_id ORDER BY current_year) AS Streak_identifier
     FROM lagged
 )
 -- Determining how many years active as well as which years are start and end of activity 
 Select actor_id, Streak_identifier,
+    MAX(quality_class) as quality_class,
     MAX(is_active) = 1 as is_active, -- this has been changed back into a boolen per WEEK 1 Lab 2
     MIN(current_year) as start_year,
     MAX(current_year) as end_year,
