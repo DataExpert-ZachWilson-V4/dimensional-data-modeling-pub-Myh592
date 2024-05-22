3f31c2bb3b7ca2c035d4883efd0b3bb6c3b8f467
Mohammad.YM <Mymah592@gmail.com>
2024-05-21 23:45:44 -0400

    Update query_2.sql
    trying

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -2,65 +2,84 @@ INSERT INTO mymah592.Actors
 WITH 
     last_year AS (
         SELECT 
-            *
+            Actor,
+            actor_ID,
+            films,
+            quality_class,
+            is_active,
+            year
         FROM
             mymah592.actors
         WHERE
             current_year = 1999
         ),
-    this_year_temp AS (
+    This_year_temp AS (
         SELECT
-            actor,
-            actor_id,
+            a.actor,
+            a.actor_id,
             year,
             -- Aggregating all films for an actor in one year
-            ARRAY_AGG(ROW(
-                year,
-                film,
-                votes,
-                rating,
-                film_id
-                )) AS films,
-            AVG(rating) AS avg_rating
+            Array_AGG(ROW(
+                a.Year,
+                a.film,
+                a.votes,
+                a.rating,
+                a.film_id
+                )) AS Films,
+                AVG(a.rating) as avg_Rating
         FROM
-            bootcamp.actor_films
+            bootcamp.actor_films a
         WHERE
-            year = 2000
+            a.year = 2000
         GROUP BY
-            actor,
-            actor_id,
-            year
-    ), 
-    this_year AS (
-        SELECT 
-            actor,
-            actor_id,
-            year,
-            films,
-            CASE 
-                WHEN avg_rating > 8 THEN 'star'
-                WHEN avg_rating > 7 THEN 'good'
-                WHEN avg_rating > 6 THEN 'average'
-                ELSE 'bad' 
-            END AS quality_class
-        FROM 
-            this_year_temp
+            a.Actor,
+            a.actor_ID,
+            a.year
+    ), -- temporary table set up data then averages calculated in following. 
+    This_year as(
+        SELECT actor,
+        actor_id,
+        year,
+        films,
+        Case when avg_Rating > 8 THEN 'star'
+        when avg_Rating > 7 then 'Good'
+        when avg_Rating > 6 then 'average'
+        else 'bad' end as quality_class
+        from This_year_temp
+
     )
+
 -- COALESCE all the values that are not changing to handle NULLS
-SELECT
-    COALESCE(ly.actor, ty.actor) AS actor,
-    COALESCE(ly.actor_id, ty.actor_id) AS actor_id,
+    SELECT
+    COALESCE(ly.actor, ty.actor) as actor,
+    COALESCE(ly.actor_ID, ty.actor_ID) as actor_ID,
     CASE 
         WHEN ty.year IS NULL THEN ly.films 
-        WHEN ty.year IS NOT NULL AND ly.films IS NULL THEN 
-            ty.films
-        WHEN ty.year IS NOT NULL AND ly.films IS NOT NULL THEN
-            ty.films || ly.films
-    END AS films,
-    COALESCE(ty.quality_class, ly.quality_class) AS quality_class,
-    ty.year IS NOT NULL AS is_active,
-    COALESCE(ty.year, ly.year + 1) AS year
-FROM 
-    last_year ly
-    FULL OUTER JOIN this_year ty
-    ON ly.actor_id = ty.actor_id
+        WHEN ty.year IS NOT NULL and ly.films IS NULL THEN 
+        Array[
+            ROW(
+            ty.year, 
+            ty.film,
+            ty.votes,
+            ty.rating,
+            ty.film_id
+            )
+        ]
+        WHEN ty.year IS NOT NULL and ly.films IS NOT NULL THEN
+        Array[
+            ROW(
+            ty.year,
+            ty.film,
+            ty.votes,
+            ty.rating,
+            ty.film_id
+            )
+            ] || ly.films
+        END as films,
+    COALESCE(ty.quality_class, ly.quality_class) as quality_class,
+    ty.year IS NOT NULL as is_active,
+    COALESCE(ty.year, ly.year + 1) as year
+    FROM last_year ly
+    FULL OUTER JOIN This_year ty
+    ON ly.actor_ID = ty.actor_ID
+
