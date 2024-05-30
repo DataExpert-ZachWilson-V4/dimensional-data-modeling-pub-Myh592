654a5aeccc529b32bb4eee284418132eaa4c1329
Mohammad.YM <Mymah592@gmail.com>
2024-05-30 19:31:21 -0400

    Update query_1.sql

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -2,9 +2,6 @@
 CREATE OR REPLACE TABLE mymah592.Actors (
     actor VARCHAR,
     actor_ID VARCHAR,
-    film VARCHAR,
-    votes INTEGER,
-    year INTEGER,
     films ARRAY<ROW(Year INTEGER, film VARCHAR, votes INTEGER, rating DOUBLE, film_id VARCHAR)>,
     quality_class VARCHAR,
     is_active BOOLEAN,
