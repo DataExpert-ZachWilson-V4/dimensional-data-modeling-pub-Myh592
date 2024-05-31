71f1cfc6b7f36e85817d8b33c01911e471036dac
Mohammad.YM <Mymah592@gmail.com>
2024-05-30 20:10:41 -0400

    Update query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -34,11 +34,13 @@ SELECT
     FROM lagged
 )
 -- Determining how many years active as well as which years are start and end of activity 
-Select actor_id, Streak_identifier,
-    quality_class,
+Select 
+    actor,
+    actor_id, 
+    MAX(quality_class) as quality_class,
     MAX(is_active) = 1 as is_active, -- this has been changed back into a boolen per WEEK 1 Lab 2
     MIN(current_year) as start_year,
     MAX(current_year) as end_year,
     2000 as current_year
     FROM streaked
-    GROUP BY actor_id, Streak_identifier
+    GROUP BY actor, actor_id, Streak_identifier
